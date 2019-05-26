26180b4dfbc1a83b628d6905cfa394d041a936c5
yuki-tashiro <>
2019-05-26 15:44:28 +0900

    add：created_at日付変換

--- apiv1/views.py
+++ apiv1/views.py
@@ -2,6 +2,8 @@
 # JSON形式のレスポンスオブジェクトの作成：
 #
 
+from datetime import datetime
+
 from rest_framework import viewsets, routers
 from scraping.models import Infomation
 from scraping.models import ReleaseInfo
@@ -13,6 +15,11 @@ class InfomationViewSet(viewsets.ReadOnlyModelViewSet):  # 情報を参照する
     # Infomationモデルの一覧GET,詳細GETを行うAPI
     # modelにDB操作のqueryを送る(filterかけて一部を取り出すとかできる)
     queryset = Infomation.objects.all().order_by("created_at").reverse()
+
+    # created_atをxxxx年yy月zz日に変換
+    for query in queryset:
+        query.created_at = query.created_at.date()
+
     serializer_class = InfomationSerializer  # serializeするクラスを指定
 
     # Infomation.objects.order_by(“-created_at”)[0:100] # 作成日順に先頭100件
